a94db7133cfd626fbb3623b8c137c199faa5e680
dam05 <dam05@PC05.aula1.com>
2019-04-25 11:14:24 +0200

    Versión 2.1 despues de clonar el proyecto de github, creamos una clase nueva y volvemos a subirlo

---
+++ src/ejemplorepositorios/NewClass.java
@@ -0,0 +1,15 @@
+package ejemplorepositorios;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dam05
+ */
+public class NewClass {
+    //esta es la clase nueva que hemos creado después de clonar el proyecto
+}
